a9393917e599b041472cec950102733e5b752107
petitviolet <violethero0820@gmail.com>
2017-07-17 21:32:03 +0900

    Id jsonify

--- modules/adapter/src/main/scala/net/petitviolet/anost/adapter/support/json/JsonSupport.scala
+++ modules/adapter/src/main/scala/net/petitviolet/anost/adapter/support/json/JsonSupport.scala
@@ -3,10 +3,17 @@ package net.petitviolet.anost.adapter.support.json
 import net.petitviolet.anost.support.Id
 import net.petitviolet.anost.usecase.post.{ PostOutput, SavePostArg }
 import net.petitviolet.anost.usecase.user.{ RegisterUserArgs, UserOutput }
-import spray.json.{ DefaultJsonProtocol, RootJsonFormat }
+import spray.json.{ DefaultJsonProtocol, DeserializationException, JsString, JsValue, RootJsonFormat }
 
 trait JsonSupport extends DefaultJsonProtocol {
-  implicit def idJson[A]: RootJsonFormat[Id[A]] = jsonFormat(Id.apply[A], "value")
+  implicit def idJson[A]: RootJsonFormat[Id[A]] = new RootJsonFormat[Id[A]] {
+    override def read(json: JsValue): Id[A] = json match {
+      case JsString(id) => Id(id)
+      case _ => throw DeserializationException("Id")
+    }
+
+    override def write(obj: Id[A]): JsValue = JsString(s"${obj.value}")
+  }
   implicit val userArgJson: RootJsonFormat[RegisterUserArgs] = jsonFormat3(RegisterUserArgs.apply)
   implicit val userOutputJson: RootJsonFormat[UserOutput] = jsonFormat3(UserOutput.apply)
   implicit val savePostArgJson: RootJsonFormat[SavePostArg] = jsonFormat4(SavePostArg.apply)
