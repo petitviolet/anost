12cb278c9a087232a66fe3caf7c8ead5606ba4aa
petitviolet <violethero0820@gmail.com>
2017-07-17 23:01:04 +0900

    let ValidationError case class

--- modules/usecase/src/main/scala/net/petitviolet/anost/usecase/exceptions.scala
+++ modules/usecase/src/main/scala/net/petitviolet/anost/usecase/exceptions.scala
@@ -2,10 +2,13 @@ package net.petitviolet.anost.usecase
 
 import scalaz.NonEmptyList
 
-class ValidationError(reason: NonEmptyList[String]) extends Exception {
+case class ValidationError(reason: NonEmptyList[String]) extends Exception {
   override def getMessage: String = {
     val msgs = reason.stream.mkString(", ")
     s"ValidationError($msgs)"
   }
 }
 
+object ValidationError {
+  def apply(msg: String): ValidationError = ValidationError(NonEmptyList(msg))
+}
