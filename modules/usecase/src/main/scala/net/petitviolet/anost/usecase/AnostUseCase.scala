7a31a4ce179d9018443549c06ec64a9b67716692
petitviolet <violethero0820@gmail.com>
2017-07-19 23:57:14 +0900

    AnostUseCase and AnostAuthUseCase

--- modules/usecase/src/main/scala/net/petitviolet/anost/usecase/AnostUseCase.scala
+++ modules/usecase/src/main/scala/net/petitviolet/anost/usecase/AnostUseCase.scala
@@ -1,9 +1,28 @@
 package net.petitviolet.anost.usecase
 
+import net.petitviolet.anost.domain.user.{ AuthTokenValue, User, UsesUserRepository }
+import net.petitviolet.anost.support.UsesLogger
 import net.petitviolet.anost.support.contracts.{ InputPort, UseCase }
 
+import scala.concurrent.Future
+import scalaz.Scalaz._
+
+// usecases for all users
 trait AnostUseCase[I <: In, O <: Out] extends UseCase with InputPort[I, O, Context] {
+}
+
+// usecases only for logged-in users
+trait AnostAuthUseCase[I <: In, O <: Out] extends AnostUseCase[AuthIn[I], O] with UsesUserRepository with UsesLogger {
+
+  final override protected def call(arg: In)(implicit ctx: Ctx): Future[Out] = {
+    import ctx._
+    (userRepository.findByToken andThenK { user => callWithUser(arg.in, user) }).run(arg.token)
+  }
+
+  protected def callWithUser(arg: I, user: User)(implicit ctx: Ctx): Future[Out]
 }
 
 trait In extends Any
 trait Out extends Any
+
+case class AuthIn[I <: In](token: AuthTokenValue, in: I) extends In
