10cd813003fcc69d4d093a70eb2991fedaeca11a
petitviolet <violethero0820@gmail.com>
2017-07-24 00:02:01 +0900

    implemented apiRequest module

---
+++ client/src/util/request.ts
@@ -0,0 +1,37 @@
+export enum HttpMethod {
+  GET = 'GET',
+  POST = 'POST',
+}
+
+const httpHeader = (token: string): Headers => {
+  if (token === '') {
+    return new Headers({
+      'Content-Type': 'application/json',
+    });
+  } else {
+    return new Headers({
+      'Content-Type': 'application/json',
+      'Authroization': `Bearer ${token}`,
+    });
+  }
+};
+
+const ENDPOINT = `http://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT || 80}`;
+
+export function apiRequest(method: HttpMethod, path: string, token: string): Promise<any> {
+  const url = `${ENDPOINT}${path}`;
+  console.log(`url: ${url}`);
+  return fetch(url, { method: method, headers: httpHeader(token) })
+  .then(response => {
+    if (response.status === 200) {
+      // chain promise
+      return response.json();
+    } else {
+      return new Error(`illegal status code: ${response.status}`);
+    }
+  })
+  .catch(error => {
+    // console.dir(error);
+    return error;
+  });
+}
