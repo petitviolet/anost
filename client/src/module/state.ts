db483fe557b01791750d5dabdee7b76b14e55b08
petitviolet <violethero0820@gmail.com>
2017-08-10 00:03:13 +0900

    add RouteComponentProps fields

--- client/src/module/state.ts
+++ client/src/module/state.ts
@@ -1,4 +1,11 @@
+import { match } from 'react-router-dom';
+import * as H from 'history';
+
 export interface State {
   loading: boolean;  // duration of sending request
   error: Error | null; // something error occurred or not
+  match?: match<any>;
+  location?: H.Location;
+  history?: H.History;
+  staticContext?: any;
 }
