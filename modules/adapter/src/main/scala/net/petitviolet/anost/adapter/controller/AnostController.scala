2e744b5367e9070d0b8c16dc9246e98dba027a2f
petitviolet <violethero0820@gmail.com>
2017-08-12 23:08:51 +0900

    allowed Authorization header

--- modules/adapter/src/main/scala/net/petitviolet/anost/adapter/controller/AnostController.scala
+++ modules/adapter/src/main/scala/net/petitviolet/anost/adapter/controller/AnostController.scala
@@ -164,12 +164,12 @@ object AnostController {
   //  val extract: Directive[(HttpRequest, IP, UserAgent)] =
   //    extractRequest & IP.extractIp & UserAgent.extractUserAgent
 
-  val defaultHeaders: Seq[HttpHeader] = {
+  val defaultHeaders: collection.immutable.Seq[HttpHeader] = {
     val server = Server.apply("anost")
     val cors = `Access-Control-Allow-Origin`(HttpOriginRange.*)
     import HttpMethods._
     val allow = `Access-Control-Allow-Methods`(GET :: POST :: HEAD :: OPTIONS :: Nil)
-    val allowHeader = `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept")
+    val allowHeader = `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Authorization")
 
     server :: cors :: allow :: allowHeader :: Nil
   }
