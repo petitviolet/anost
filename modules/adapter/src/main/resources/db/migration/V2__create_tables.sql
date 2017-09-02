600feeb0b12497c4ec5bf1c1bbb1d448a5707003
petitviolet <violethero0820@gmail.com>
2017-09-02 22:19:30 +0900

    added comments table schema

--- modules/adapter/src/main/resources/db/migration/V2__create_tables.sql
+++ modules/adapter/src/main/resources/db/migration/V2__create_tables.sql
@@ -9,3 +9,16 @@ CREATE TABLE posts (
     PRIMARY KEY (id),
     FOREIGN KEY (owner_id) REFERENCES users(id)
 );
+
+CREATE TABLE comments (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    post_id VARCHAR(36) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    sentence TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT NOW(),
+    updated_at DATETIME NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (id),
+    INDEX(post_id),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
