e7be2163d683a2e29b290d63856f52162cfe8e48
petitviolet <violethero0820@gmail.com>
2017-10-09 17:39:21 +0900

    use bootstrap Button

--- client/src/component/Header.tsx
+++ client/src/component/Header.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Logout } from './Logout';
 import { UserProps } from '../module/';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 export const Header: React.StatelessComponent<UserProps> =
   (props) => {
@@ -11,9 +12,13 @@ export const Header: React.StatelessComponent<UserProps> =
         Anost!
         {(props.value.user && props.value.login)
           ? <div className="header-item-component">
-              <StartPostCreateComponent /><Logout {...props} />
-            </div>
-          : <div className="header-item-component"><Link className="header-item" to="/login">Login</Link></div>
+            <StartPostCreateComponent /><Logout {...props} />
+          </div>
+          : <div className="header-item-component">
+            <Link className="header-item" to="/login">
+              <Button>Log in</Button>
+            </Link>
+          </div>
         }
       </div>
     );
@@ -23,7 +28,7 @@ const StartPostCreateComponent: React.StatelessComponent<{}> =
   (props: {}) => {
     const path = `/posts/new`;
     return (
-      <Link to={path} className="header-item">New Post</Link>
+      <Link to={path} className="header-item"><Button>New Post</Button></Link>
     );
   }
 
--- client/src/component/Logout.tsx
+++ client/src/component/Logout.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { UserProps } from '../module';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export const Logout: React.StatelessComponent<UserProps> =
   (props: UserProps) => {
     return (
-      <Link to="/" onClick={() => props.actions.logout()} className="header-item">Log out</Link>
+      <Link to="/" onClick={() => props.actions.logout()} className="header-item"><Button>Log out</Button></Link>
     );
   };
